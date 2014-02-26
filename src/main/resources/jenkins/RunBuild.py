2beb338c11fe92d4f1d70f84ba8961b4c0dd1366
Andrew Phillips <demobox1@yahoo.co.uk>
2014-02-26 14:35:31 -0700

    Cleaning up BuildJob.py

--- src/main/resources/jenkins/RunBuild.py
+++ src/main/resources/jenkins/RunBuild.py
@@ -3,13 +3,13 @@ from java.util import Properties
 from java.io import StringReader
 from com.xebialabs.xlrelease.plugins.ci.jenkins import JenkinsBuildInvocation
 
-print "Going to build jenkins job %s \n" %jobName
-print "with parameters: %s. \n" %parameters
+print "About to run Jenkins build for job '%s'\n" % (jobName)
+print "Build parameters: '%s'\n" % (parameters)
 props = Properties()
 props.load(StringReader(parameters))
 
-buildJob = JenkinsBuildInvocation(jenkinsUrl, keyFile, password,jobName,props)
-result = buildJob.run()
-print "Build result is %s. \n" %result
+build = JenkinsBuildInvocation(jenkinsUrl, keyFile, password, jobName, props)
+exitCode = build.run()
+print "Build exit code is: %s\n" % (exitCode)
 
-sys.exit(result)
+sys.exit(exitCode)
