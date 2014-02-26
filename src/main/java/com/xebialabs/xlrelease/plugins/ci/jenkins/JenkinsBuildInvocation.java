30e2c792ac74095f8b829de60f1fc8d9a88485d1
Andrew Phillips <demobox1@yahoo.co.uk>
2014-02-26 14:27:04 -0700

    Cleaning up JenkinsBuildInvocation

--- src/main/java/com/xebialabs/xlrelease/plugins/ci/jenkins/JenkinsBuildInvocation.java
+++ src/main/java/com/xebialabs/xlrelease/plugins/ci/jenkins/JenkinsBuildInvocation.java
@@ -14,15 +14,15 @@ import java.util.Properties;
 import hudson.cli.CLI;
 
 public class JenkinsBuildInvocation {
+    private static final String CLI_BUILD_COMMAND = "build";
 
-    private String url;
-    private String keyFile;
-    private String password;
-    private String jobName;
-    private Properties parameters;
-    private static final String BUILD_COMMAND = "build";
+    private final String url;
+    private final String keyFile;
+    private final String password;
+    private final String jobName;
+    private final Properties parameters;
 
-    public JenkinsBuildInvocation(final String url, final String keyFile, final String password, final String jobName, final Properties parameters) {
+    public JenkinsBuildInvocation(String url, String keyFile, String password, String jobName, Properties parameters) {
         this.url = url;
         this.keyFile = keyFile;
         this.password = password;
@@ -31,38 +31,32 @@ public class JenkinsBuildInvocation {
     }
 
     public int run() {
-
         try {
             CLI cli = new CLI(new URL(url));
             KeyPair key = CLI.loadKey(new File(keyFile), password);
             cli.authenticate(Collections.singleton(key));
             cli.upgrade();
             return cli.execute(getArgs(), System.in, System.out, System.err);
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException | GeneralSecurityException e) {
+            System.err.println("Error invoking Jenkins build: " + e.getMessage());
             e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (GeneralSecurityException e) {
-            e.printStackTrace();
+            return 1;
         }
-        return -1;
-
     }
 
     private List<String> getArgs() {
         // build JOB [-c] [-f] [-p] [-r N] [-s] [-v] [-w]
-        List<String> arguments = new ArrayList<String>();
-        arguments.add(BUILD_COMMAND);
+        List<String> arguments = new ArrayList<>();
+        arguments.add(CLI_BUILD_COMMAND);
         arguments.add(jobName);
-        for(String key : parameters.stringPropertyNames())
-        {
+        for(String key : parameters.stringPropertyNames()) {
             arguments.add("-p");
-            arguments.add(key+"="+parameters.getProperty(key));
+            arguments.add(String.format("%s=%s", key, parameters.getProperty(key)));
         }
+        // TODO: make configurable?
         arguments.add("-s");
         arguments.add("-v");
         arguments.add("-w");
-
         return arguments;
     }
 
@@ -70,31 +64,15 @@ public class JenkinsBuildInvocation {
         return url;
     }
 
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
     public String getKeyFile() {
         return keyFile;
-    }
-
-    public void setKeyFile(String keyFile) {
-        this.keyFile = keyFile;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getJobName() {
         return jobName;
-    }
-
-    public void setJobName(final String jobName) {
-        this.jobName = jobName;
     }
 }
