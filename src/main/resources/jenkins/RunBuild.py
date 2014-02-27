92875490babc1a84fc94a2076ea23f53d98498c9
Andrew Phillips <demobox1@yahoo.co.uk>
2014-02-27 01:21:56 -0500

    Avoiding an NPE if the optional property 'parameters' is empty

--- src/main/resources/jenkins/RunBuild.py
+++ src/main/resources/jenkins/RunBuild.py
@@ -4,9 +4,11 @@ from java.io import StringReader
 from com.xebialabs.xlrelease.plugins.ci.jenkins import JenkinsBuildInvocation
 
 print "About to run Jenkins build for job '%s'\n" % (jobName)
-print "Build parameters: '%s'\n" % (parameters)
+
 props = Properties()
-props.load(StringReader(parameters))
+if parameters is not None:
+  print "Build parameters: '%s'\n" % (parameters)
+  props.load(StringReader(parameters))
 
 build = JenkinsBuildInvocation(jenkinsUrl, keyFile, password, jobName, props)
 exitCode = build.run()
