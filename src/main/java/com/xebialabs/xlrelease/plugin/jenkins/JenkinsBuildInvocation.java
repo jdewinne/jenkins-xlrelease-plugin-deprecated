f3b651362b1c5e3dbe9a8b03ac409efe970f14e9
Andrew Phillips <demobox1@yahoo.co.uk>
2014-02-28 23:27:50 -0500

    Additional comments explaining the CLI build command

--- src/main/java/com/xebialabs/xlrelease/plugin/jenkins/JenkinsBuildInvocation.java
+++ src/main/java/com/xebialabs/xlrelease/plugin/jenkins/JenkinsBuildInvocation.java
@@ -52,7 +52,19 @@ public class JenkinsBuildInvocation {
     }
 
     private List<String> getArgs() {
-        // build JOB [-c] [-f] [-p] [-r N] [-s] [-v] [-w]
+        /*
+         * build JOB [-c] [-f] [-p] [-r N] [-s] [-v] [-w]
+         *
+         * JOB    : Name of the job to build
+         * -c     : Check for SCM changes before starting the build, and if there's
+         *          no change, exit without doing a build
+         * -p     : Specify the build parameters in the key=value format.
+         * -r VAL : Number of times to retry reading of the output log if it does not
+         *          exists on first attempt. Defaults to 0. Use with -v.
+         * -s     : Wait until the completion/abortion of the command
+         * -v     : Prints out the console output of the build. Use with -s
+         * -w     : Wait until the start of the command
+         */
         List<String> arguments = new ArrayList<>();
         arguments.add(CLI_BUILD_COMMAND);
         arguments.add(jobName);
@@ -60,7 +72,6 @@ public class JenkinsBuildInvocation {
             arguments.add("-p");
             arguments.add(String.format("%s=%s", key, parameters.getProperty(key)));
         }
-        // TODO: make configurable?
         arguments.add("-s");
         arguments.add("-v");
         arguments.add("-w");
